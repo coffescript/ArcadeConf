843334114999701aae772831a6dc3b1b29240a64
Deiby Mejia <dmejia1204@gmail.com>
2020-04-26 23:05:24 -0600

    create of activities and fragments for application

---
+++ app/src/main/java/com/example/arcadeconf/ui/activities/SplashScreenActivity.kt
@@ -0,0 +1,13 @@
+package com.example.arcadeconf.ui.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.arcadeconf.R
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
